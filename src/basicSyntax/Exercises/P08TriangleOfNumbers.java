6551ada8ee3ca0475c6b131039ffd22da810d16d
Kaloy4n <kaloyan.braskov@gmail.com>
2022-09-24 21:52:14 +0300

    Adding basicSyntax Solutions to Exercises
    Adding basicSyntax Solutions to Exercises

---
+++ src/basicSyntax/Exercises/P08TriangleOfNumbers.java
@@ -0,0 +1,18 @@
+package basicSyntax.exercise;
+
+import java.util.Scanner;
+
+public class P08TriangleOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        //колко пъти да печатам ред, докато броя е по-малък или равен на входящия брой
+        for (int row = 1; row <= n ; row++) {
+            //колко пъти да печатам колонка, докато е по-малка и равна на броя редове
+            for (int count = 1; count <= row ; count++) {
+                System.out.print(row + " ");
+            }
+            System.out.println();
+        }
+    }
+}
